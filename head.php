be4f6a4eb3d89547b6dd460d7029cecd6c12042b
bloodmanqk <hrr_95@abv.bg>
2013-05-09 22:04:47 +0300

    First

---
+++ head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>PGEHT-Pleven</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Myriad_Pro_300.font.js" type="text/javascript"></script>
+<!--[if lt IE 7]>
+<script type="text/javascript" src="js/ie_png.js"></script>
+<script type="text/javascript">ie_png.fix('.png, #header .row-2 ul li a, #content, .list li');</script>
+<![endif]-->
+</head>
+<body id="page1">
+<div class="tail-top">
+  <div class="tail-bottom">
+    <div class="body-bg">
+      <!-- HEADER -->
+      <div id="header">
+        <div class="row-1">
+		
+          <div class="fleft"><a href="#"><img src="images/logo.png" alt="" /></a></div>
+          <div class="fright"><img src="images/phone.png" alt="" /></div>
+		  <div class="fright">login shit</div>
+        </div>
+        <div class="row-2">
+          <ul>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include "head.php" ?>
+            <li class="m1"><a href="index.php" class="active">Начало</a></li>
+            <li class="m3"><a href="about-us.php">За родители</a></li>
+            <li class="m6"><a href="services.php">За ученици</a></li>
+            <li class="m4"><a href="support.php">За ПГЕХТ</a></li>
+            <li class="m5"><a href="contact-us.php">За учители</a></li>
+            <li class="m6"><a href="sitemap.php">Галерия</a></li>
+			<li class="m3"><a href="sitemap.php">Администрация</a></li>
+			<li class="m4"><a href="sitemap.php">Галерия</a></li>
+			
+          </ul>
+        </div>
+        <div class="row-3">
+               
+                  <strong><a href="#">Специалности</a></strong>
+                  <li><strong><a href="#">Прием</a></strong></br><a href="#">Информация</a></br><a href="#">Класиране</a></li>
+                  <li><strong><a href="#">Форум</a></strong>Форумът е в разработка.</li>
+                
+        </div>
+      </div>
+      <!-- CONTENT -->
+      <div id="content">
+        <div class="tail-right">
+          <div class="wrapper">
+            <div class="col-1">
+              <div class="indent">
+                <div class="indent1">
+                  <h2>Професионална гимназия по електроника и химични технологии 'Проф. Асен Златаров'</h2>
+				  <img src="images/pg.jpg" alt="" />
+                  <p> Професионална гимназия по електроника и химични технологии 'Проф. Асен Златаров' е разположена в Плевен (Централна Северна България). 
+				  Подготвя кадри в областта на Компютърната техника и технологии, Компютърни мрежи, Възобновяеми енергийни източници и химични технологии. 
+				  Броят на учениците, обучаващи се в училище, е 400. Обучението се провежда в 20 специализирани кабинета, оборудвани с мултимедийни системи, 
+				  12 лаборатории по компютърна техника, 6 кабинета по информатика и информационни технологии, 2 лаборатории по фото- волтаични системи.</p>
+                  </div>
+              </div>
+            </div>
+<?php include "footer.php" ?>
